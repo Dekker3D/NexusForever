6a5b694e9b8442b586a096591f259fd6f15b9727
Dekker3D <dekker3d@gmail.com>
2021-06-14 21:07:02 +0200

    Fixed some stuff to make friends lists work.

--- Source/NexusForever.Database.Character/CharacterDatabase.cs
+++ Source/NexusForever.Database.Character/CharacterDatabase.cs
@@ -239,14 +239,12 @@ namespace NexusForever.Database.Character
         /// <summary>
         /// Used by the Global Contact Manager to get the next unique ID.
         /// </summary>
-        public ulong GetNextContactId()
+        public ulong? GetNextContactId()
         {
             using var context = new CharacterContext(config);
 
             return context.CharacterContact
-                .GroupBy(i => 1)
-                .Select(g => g.Max(s => s.OwnerId))
-                .ToList()[0];
+                .FirstOrDefault()?.ContactId;
         }
 
         public async Task<List<CharacterContactModel>> GetPendingContactRequests(ulong characterId)
